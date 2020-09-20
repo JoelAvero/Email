a4f35e78639a86a3c556d9a8aeb107662e20814f
Joel <joelavero@gmail.com>
2020-09-20 20:53:12 -0300

    added reply, archived, read, and some functions more

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -3,14 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => inbox());
   document.querySelector('#sent').addEventListener('click', () => sent());
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#archived').addEventListener('click', () => archive());
   document.querySelector('#compose').addEventListener('click', compose_email);
 
   document.querySelector('#compose-form').onsubmit = send_mail;
 
+  
   // By default, load the inbox
   inbox();
 });
+
+var replyy = false;
 
 function compose_email() {
 
@@ -18,11 +21,13 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  if (window.replyy == false){
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
-
+  }
+  window.replyy = false
 }
 
 function load_mailbox(mailbox) {
@@ -34,6 +39,9 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#title').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+
+
 
 function send_mail() {
 
@@ -57,9 +65,10 @@ function send_mail() {
   
   alert('Success!');
   sent();
+  
   return false;
-
 }
+
 
 
 function inbox() {
@@ -77,53 +86,56 @@ function inbox() {
   .then(mails => {
     console.log(mails);
     mails.forEach(mail => {
-      
-      newdiv.innerHTML += `
-      
-      <div class="card">
-        <div class="card-header" id="heading${mail.id}">
-          <h2 class="mb-0">
-            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse${mail.id}" >
+        let color
+        if(mail.read == true){color = "lightgrey"}else{color = ""}
+        newdiv.innerHTML += `
+        
+        <div class="card">
+          <div class="card-header" id="heading${mail.id}" style="background-color: ${color};">
+            <h2 class="mb-0">
+              <button onclick='read(${mail.id})' class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse${mail.id}" >
+                <div class='container'>
+                  <div class='row'>
+                    <div class='col-4'><strong>From:</strong> ${mail.sender}</div>
+                    <div class='col-5'><strong>Subject:</strong> ${mail.subject}</div>
+                    <div class='col-3'><span style='font-size: 14px;'><strong>Date:</strong> ${mail.timestamp}</span></div>
+                  </div>
+                </div>    
+              </button>
+            </h2>
+          </div>
+
+          <div id="collapse${mail.id}" class="collapse" data-parent="#inboxdiv">
+            <div class="card-body">
               <div class='container'>
                 <div class='row'>
-                  <div class='col-4'><strong>From:</strong> ${mail.sender}</div>
-                  <div class='col-5'><strong>Subject:</strong> ${mail.subject}</div>
-                  <div class='col-3'><span style='font-size: 14px;'><strong>Date:</strong> ${mail.timestamp}</span></div>
+                  <div class='col-12'><strong>From:</strong> ${mail.sender}</div>
+                  <div class='col-12'><strong>To:</strong> ${mail.recipients}</div>
+                  <div class='col-12'><strong>Subject:</strong> ${mail.subject}</div>
+                  <div class='col-12'><strong>Date:</strong> ${mail.timestamp}</div>  
                 </div>
-              </div>    
-            </button>
-          </h2>
-        </div>
-
-        <div id="collapse${mail.id}" class="collapse" data-parent="#inboxdiv">
-          <div class="card-body">
-            <div class='container'>
-              <div class='row'>
-                <div class='col-12'><strong>From:</strong> ${mail.sender}</div>
-                <div class='col-12'><strong>To:</strong> ${mail.recipients}</div>
-                <div class='col-12'><strong>Subject:</strong> ${mail.subject}</div>
-                <div class='col-12'><strong>Date:</strong> ${mail.timestamp}</div>  
               </div>
-            </div>
-            <hr>
-            <div class='container'>
-              <div class='row'>
-                <div class='col-12' style='font-size: 20px;'><strong>Message:</strong></div>
-                <div class='col-12'>${mail.body}</div>
-                <div class='col-10'></div>
-                <div class='col-2'><button class="btn btn-primary" id="reply">Reply</button></div>
+              <hr>
+              <div class='container'>
+                <div class='row'>
+                  <div class='col-12' style='font-size: 20px;'><strong>Message:</strong></div>
+                  <div class='col-12'>${mail.body}</div>
+                  <div class='col-9'></div>
+                  <div class='col-3'><button class="btn btn-primary" onclick='reply(${mail.id})' id="reply">Reply</button>
+                  <button class="btn btn-success" onclick='addArchive(${mail.id})' id="archive">Archive</button></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      
-      `
+        
+        `
+    
     });
 
   })
   
-  load_mailbox('inbox');
+  return load_mailbox('inbox');
 }
 
 
@@ -188,4 +200,129 @@ function sent(){
   })
 
   load_mailbox('sent')
+}
+
+
+function archive() {
+
+  const content = document.createElement('div');
+  content.setAttribute('class', 'accordion');
+  content.id = 'inboxdiv';
+  document.querySelector('#content').append(content);
+  
+  const newdiv = document.querySelector('#inboxdiv')
+  newdiv.innerHTML = ''
+
+  fetch('emails/archive')
+  .then(resp => resp.json())
+  .then(mails => {
+
+    console.log(mails);
+    mails.forEach(mail => {
+      
+      
+      newdiv.innerHTML += `
+      
+      <div class="card">
+        <div class="card-header" id="heading${mail.id}">
+          <h2 class="mb-0">
+            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse${mail.id}" >
+              <div class='container'>
+                <div class='row'>
+                  <div class='col-4'><strong>From:</strong> ${mail.sender}</div>
+                  <div class='col-5'><strong>Subject:</strong> ${mail.subject}</div>
+                  <div class='col-3'><span style='font-size: 14px;'><strong>Date:</strong> ${mail.timestamp}</span></div>
+                </div>
+              </div>    
+            </button>
+          </h2>
+        </div>
+
+        <div id="collapse${mail.id}" class="collapse" data-parent="#inboxdiv">
+          <div class="card-body">
+            <div class='container'>
+              <div class='row'>
+                <div class='col-12'><strong>From:</strong> ${mail.sender}</div>
+                <div class='col-12'><strong>To:</strong> ${mail.recipients}</div>
+                <div class='col-12'><strong>Subject:</strong> ${mail.subject}</div>
+                <div class='col-12'><strong>Date:</strong> ${mail.timestamp}</div>  
+              </div>
+            </div>
+            <hr>
+            <div class='container'>
+              <div class='row'>
+                <div class='col-12' style='font-size: 20px;'><strong>Message:</strong></div>
+                <div class='col-12'>${mail.body}</div>
+                <div class='col-10'></div>
+                <div class='col-3'><button class="btn btn-primary" id="reply">Reply</button>
+                <button class="btn btn-success" onclick='removeArchive(${mail.id})' id="archive">Unarchive</button></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      `
+    
+
+    });
+
+  })
+  
+  load_mailbox('archive');
+}
+
+
+function addArchive(id) {
+
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: true
+    })
+  });
+  alert('archived')
+  return inbox();
+}
+
+
+function removeArchive(id) {
+
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: false
+    })
+  });
+  alert('unarchived')
+  return archive();
+}
+
+
+
+function reply(id) {
+  
+
+  fetch(`emails/${id}`)
+  .then(resp => resp.json())
+  .then(mail => {
+    document.querySelector('#compose-recipients').value = mail.sender;
+    document.querySelector('#compose-subject').value = `Re: ${mail.subject}`;
+    document.querySelector('#compose-body').value = `${mail.timestamp}, ${mail.sender} WROTE: ${mail.body}\nREPLY: `;
+  });
+  
+  window.replyy = true;
+  compose_email();
+
+}
+
+
+function read(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  });
+
 }
